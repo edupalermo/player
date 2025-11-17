1f5911c9a2440dc6f72ddf8cdbadbf70a71a7612
Eduardo Palermo <eduardo.palermo@uniscon.com>
2025-11-17 02:19:16 +0000

    Change

--- src/main/java/org/palermo/totalbattle/player/PlayerRunnable.java
+++ src/main/java/org/palermo/totalbattle/player/PlayerRunnable.java
@@ -24,7 +24,6 @@ public class PlayerRunnable implements Runnable {
                 .password("Alemanha79")
                 .hasHelen(true)
                 .build());
-        /*
         players.add(Player.builder()
                 .name("Peter II")
                 .profileFolder("chrome-profiles/peter_ii")
@@ -49,7 +48,6 @@ public class PlayerRunnable implements Runnable {
                 .username("edupalermo+03@gmail.com")
                 .password("Alemanha79")
                 .build());
-         */
     }
 
 
