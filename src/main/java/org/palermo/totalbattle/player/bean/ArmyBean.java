681c12a0c1578fbc905591e4a35d51ea157da3a1
Eduardo Palermo <eduardo.palermo@uniscon.com>
2025-11-18 00:01:28 +0000

    Change

--- src/main/java/org/palermo/totalbattle/player/bean/ArmyBean.java
+++ src/main/java/org/palermo/totalbattle/player/bean/ArmyBean.java
@@ -1,10 +1,13 @@
 package org.palermo.totalbattle.player.bean;
 
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonPOJOBuilder;
 import lombok.Builder;
 import lombok.Getter;
 
 @Builder
 @Getter
+@JsonDeserialize(builder = ArmyBean.ArmyBeanBuilder.class)
 public class ArmyBean {
 
     private final String playerName;
@@ -12,4 +15,7 @@ public class ArmyBean {
     private final int leadership;
     private final int dominance;
     private final int authority;
+
+    @JsonPOJOBuilder(withPrefix = "")
+    public static class ArmyBeanBuilder {}
 }
