6d1c35b0a9b44cec4188607dba548bce32ac762e
Eduardo Palermo <eduardo.palermo@uniscon.com>
2025-11-28 15:53:10 +0000

    Change

--- src/main/java/org/palermo/totalbattle/player/AddressSelector.java
+++ src/main/java/org/palermo/totalbattle/player/AddressSelector.java
@@ -44,10 +44,10 @@ public class AddressSelector {
 
 
     private static final String[] ONE_TIME_BONUS = new String[] {
-            "https://totalbattle.com/de/?present=ref5_november_25_ZN6z",
-            "https://totalbattle.com/de/?present=ref1_november_25_rGga",
-            "https://totalbattle.com/de/?present=ref1_october_25_6nhH",
-            "https://totalbattle.com/de/?present=ref4_spareoctober_25_FK5y"
+            "https://totalbattle.com/en/?present=ref5_november_25_ZN6z",
+            "https://totalbattle.com/en/?present=ref1_november_25_rGga",
+            "https://totalbattle.com/en/?present=ref1_october_25_6nhH",
+            "https://totalbattle.com/en/?present=ref4_spareoctober_25_FK5y"
     };
 
     private static String getAddressByWeekDay() {
