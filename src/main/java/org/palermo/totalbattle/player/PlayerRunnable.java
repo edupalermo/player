3be5ec2a9aad82cb1e4518bfde9a3fbc189aec73
Eduardo Palermo <eduardo.palermo@uniscon.com>
2025-12-07 08:32:49 +0000

    change

--- src/main/java/org/palermo/totalbattle/player/PlayerRunnable.java
+++ src/main/java/org/palermo/totalbattle/player/PlayerRunnable.java
@@ -103,8 +103,11 @@ public class PlayerRunnable implements Runnable {
                 String os = System.getProperty("os.name").toLowerCase();
                 if (os.contains("win")) {
                     try {
-                        new ProcessBuilder("taskkill", "/IM", "chrome.exe", "/F").start();
+                        Process killerProcess = new ProcessBuilder("taskkill", "/IM", "chrome.exe", "/F").start();
+                        killerProcess.waitFor();
                     } catch (IOException e) {
+                        throw new RuntimeException(e);
+                    } catch (InterruptedException e) {
                         throw new RuntimeException(e);
                     }
                 }
