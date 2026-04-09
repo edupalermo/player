180a45b24a5669e54a8a65f34a4e45f72581e7c4
Eduardo Palermo <eduardo.palermo@uniscon.com>
2026-04-09 11:50:59 +0200

    Change

--- src/main/java/org/palermo/totalbattle/selenium/leadership/Backend.java
+++ src/main/java/org/palermo/totalbattle/selenium/leadership/Backend.java
@@ -517,7 +517,7 @@ public class Backend {
                 }
             });
             
-            final int delta = 50;
+            final int delta = 40;
 
             if (processed.size() < stack.size()) {
                 Point scroolBar = Point.of(closeButtonLocation, Point.of(1438, 356), Point.of(978, 448));
