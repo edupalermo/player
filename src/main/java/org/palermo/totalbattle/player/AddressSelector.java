c6f1da8551a0239b03aca50a3736fcce6ac8f00a
Eduardo Palermo <eduardo.palermo@uniscon.com>
2026-04-18 06:32:43 +0200

    Change

--- src/main/java/org/palermo/totalbattle/player/AddressSelector.java
+++ src/main/java/org/palermo/totalbattle/player/AddressSelector.java
@@ -49,7 +49,7 @@ public class AddressSelector {
             "https://totalbattle.com/en/?present=ref2_march_26_s3vS",
             "https://totalbattle.com/en/?present=ref3_february_26_co8P",
             "https://totalbattle.com/en/?present=ref1_february_26_2NQy",
-            "https://totalbattle.com/de/?present=ref4_january_26_Zr1R",
+            "https://totalbattle.com/en/?present=ref4_january_26_Zr1R",
             "https://totalbattle.com/en/?present=merry_christmas_2025",
             "https://totalbattle.com/en/?present=ref3_december_25_u2R8",
             "https://totalbattle.com/en/?present=ref2_december_25_FYWg",
